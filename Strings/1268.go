fe3d69b7b4f01b6e9f832da053523a565bfa5b00
unknown <abhishekm2610@gmail.com>
2022-06-13 01:41:06 +0530

    Solved 1268

---
+++ Strings/1268.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//Original Solution
+func suggestedProducts(products []string, searchWord string) [][]string {
+	outputArray := [][]string{}
+	var searchString = ""
+	for _, char := range searchWord {
+		searchString += string(char)
+		listOfMatches := []string{}
+		for _, value := range products {
+			if len(value) >= len(searchString) {
+				if searchString == value[:len(searchString)] {
+					listOfMatches = append(listOfMatches, string(value))
+				}
+			} else if value == searchString {
+				listOfMatches = append(listOfMatches, string(value))
+			}
+
+		}
+		sort.Strings(listOfMatches)
+		if len(listOfMatches) < 3 {
+			outputArray = append(outputArray, listOfMatches)
+		} else {
+			outputArray = append(outputArray, listOfMatches[:3])
+		}
+	}
+	return outputArray
+}
+
+func main() {
+	input1 := []string{"bags", "baggage", "banner", "box", "cloths"}
+	input2 := "bags"
+
+	fmt.Println(suggestedProducts(input1, input2))
+}
