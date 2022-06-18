b7f1be09bd094f9093968fc3030f3e2a6c06fa2d
unknown <abhishekm2610@gmail.com>
2022-06-18 20:04:52 +0530

    Solved 35

--- Algorithm/Study Plan 1/35.go
+++ Algorithm/Study Plan 1/35.go
@@ -4,47 +4,25 @@ import "fmt"
 
 //Original Solution
 func searchInsert(nums []int, target int) int {
-	if nums[len(nums)/2] == target {
-		return len(nums) / 2
-	}
 
 	left := 0
 	right := len(nums) - 1
 
 	for left <= right {
-		pivot := left + (right-left)/2
 
-		if nums[pivot] == target {
-			return pivot
-		}
-		if nums[pivot]-1 == target {
-			if pivot == 0 {
-				return 0
-			} else {
-				if nums[pivot-1] != target {
-					return pivot
-				}
-			}
-		}
-		if nums[pivot]+1 == target {
-			if pivot == len(nums)-1 {
-				return pivot + 1
-			} else {
-				if nums[pivot+1] != target {
-					return pivot + 1
-				}
-			}
+		mid := left + (right-left)/2
 
-		}
+		if nums[mid] == target {
 
-		if nums[pivot] < target {
-			left = pivot + 1
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
 		} else {
-			right = pivot - 1
-		}
 
+			right = mid - 1
+		}
 	}
-	return -1
+	return left
 }
 
 func main() {
